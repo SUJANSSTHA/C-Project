fda986933fa1c359a370094980644cb4cf4e79ae
SUJAN <123sujanpr@gmail.com>
2022-12-09 07:08:45 +0545

    Binary

--- c all practice/DSA practice/DSA lab/TREE/01_Binary.c
+++ c all practice/DSA practice/DSA lab/TREE/01_Binary.c
@@ -26,6 +26,54 @@ struct tree * getnode(int data)
     return ptrnew;
 }
 
+void insert(struct tree *ptrthis, int data)
+{
+    if(ptrthis==NULL)
+    root =getnode(data);
+    else if(ptrthis->info==data)
+    printf("Duplicate empty");
+    else if (data<ptrthis->info) //  if data is less the info
+    {
+        if(ptrthis->left==NULL) // IF current node has no left son then make new node as its left son
+        ptrthis->left=getnode(data);
+        else
+        insert(ptrthis->left,data); // data pass to left
 
+    }
+    else {
+
+        // if(ptrthis->info==data)
+        //  printf("Duplicate empty");
+    // else if (data<ptrthis->info) //  if data is less the info
+
+
+
+        if(ptrthis->right==NULL) // if current node has no right son
+        ptrthis->right=getnode(data); // then make new node as its right son
+        else
+        insert (ptrthis->right,data); // data pass to right
+    }
+
+
+}
+void inorder(struct tree *ptrthis)
+{
+
+}
+
+int main()
+{
+    int i, n;
+    for(i=0;i<10;i++)
+    {
+        printf("Enter a number");
+        scanf("%d", &n);
+        inset (root,n); // first put data in 'n' and send 
+    }
+    printf("Tree constructed . \n the binary search tree is \n");
+    inorder(root);
+    return 0;
+    // getch();
+}
 
 

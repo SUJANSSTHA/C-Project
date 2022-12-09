d58d8d8b943463449d70beb7425a36785fe4a30e
SUJAN <123sujanpr@gmail.com>
2022-12-09 06:42:29 +0545

    java

---
+++ c all practice/DSA practice/DSA lab/TREE/01_Binary.c
@@ -0,0 +1,13 @@
+// read the first number 
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+
+struct node // tree node;
+{
+    int info;
+    struct node*left;
+    struct node*right;
+};
+
+

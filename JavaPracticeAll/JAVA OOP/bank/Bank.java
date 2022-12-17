788ec2b7aced095fc1602fea0e81e67e3b7cbde0
SUJAN <123sujanpr@gmail.com>
2022-12-17 21:57:58 +0545

    deleted bank

--- JavaPracticeAll/JAVA OOP/bank/Bank.java
+++
@@ -1,7 +0,0 @@
-package bank;
-
-public class Bank {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
--- JavaPracticeAll/JAVA OOP/bank/suj.java
+++
@@ -1,7 +0,0 @@
-package bank;
-
-public class suj {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
